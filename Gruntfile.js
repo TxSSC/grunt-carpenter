b7e174e8e1349f501032dfcabdd6b80a2bee1098
Matt McFarland <matmcfarland@gmail.com>
2013-10-17 14:08:44 -0500

    clean up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,68 +6,56 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
 module.exports = function(grunt) {
 
-  // Project configuration.
+  /**
+   * Grunt configuration
+   */
+
   grunt.initConfig({
     jshint: {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>',
+        '<%= nodeunit.tests %>'
       ],
       options: {
-        jshintrc: '.jshintrc',
-      },
+        jshintrc: '.jshintrc'
+      }
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ['tmp']
     },
 
-    // Configuration to be run (and then tested).
-    carpenter: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
-
-    // Unit tests.
     nodeunit: {
-      tests: ['test/*_test.js'],
+      tests: ['test/index.js']
     },
 
+    carpenter: {
+      base: {
+        options: {
+          templatePath: "test/fixtures/templates"
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'test/fixtures/',
+            src: ['content/**/*.md', 'content/**/*.html'],
+            dest: 'tmp',
+          }
+        ]
+      }
+    }
   });
 
-  // Actually load this plugin's task(s).
+  // Load tasks
   grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
+  // Define tasks
   grunt.registerTask('test', ['clean', 'carpenter', 'nodeunit']);
-
-  // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
-
 };
